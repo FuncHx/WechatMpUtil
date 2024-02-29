ee4836d1c96802f55cadac6444883d05aa1b31b6
bettleChen <2207153529@qq.com>
2024-02-29 18:23:05 +0800

    更新SQL文件

--- data.sql
+++ data.sql
@@ -107,11 +107,6 @@ CREATE TABLE `sys_user` (
     `status` char(1) DEFAULT '0' COMMENT '帐号状态（0正常 1停用）',
     `del_flag` char(1) DEFAULT '0' COMMENT '删除标志（0代表存在 2代表删除）',
     `login_ip` varchar(50) DEFAULT '' COMMENT '最后登录IP',
-    `login_date` datetime DEFAULT NULL COMMENT '最后登录时间',
-    `create_by` varchar(64) DEFAULT '' COMMENT '创建者',
-    `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-    `update_by` varchar(64) DEFAULT '' COMMENT '更新者',
-    `update_time` datetime DEFAULT NULL COMMENT '更新时间',
-    `remark` varchar(500) DEFAULT NULL COMMENT '备注',
+    `login_date` datetime DEFAULT NULL COMMENT '最后登录时间'
     PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户信息表';
